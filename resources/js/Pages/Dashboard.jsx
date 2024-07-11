837607a4562dfe40c6413869f5ede8dac08f7919
remoaldy20 <remoaldy@gmail.com>
2024-07-11 16:20:20 +0700

    tes commit

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -13,19 +13,19 @@ export default function Dashboard({ auth }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     
                         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-4">
-                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4 hover:scale-105 duration-300">
+                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4 hover:scale-105 hover:text-sky-400 duration-300">
                                 <h3 className="text-lg font-medium text-gray-900">Total Admin</h3>
                                 <p className="text-gray-600">2</p>
                             </div>
-                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4">
+                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4 hover:scale-105 hover:text-sky-400 duration-300">
                                 <h3 className="text-lg font-medium text-gray-900">Total siswa</h3>
                                 <p className="text-gray-600">154</p>
                             </div>
-                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4">
+                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4 hover:scale-105 hover:text-sky-400 duration-300">
                                 <h3 className="text-lg font-medium text-gray-900">Total kursus</h3>
                                 <p className="text-gray-600">25</p>
                             </div>
-                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4">
+                            <div className="bg-white shadow overflow-hidden sm:rounded-lg p-4 hover:scale-105 hover:text-sky-400 duration-300">
                                 <h3 className="text-lg font-medium text-gray-900">Total petugas</h3>
                                 <p className="text-gray-600">10</p>
                             </div>
