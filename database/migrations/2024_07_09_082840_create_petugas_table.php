f60aba10caa00c545060fcadf8f57bb4157cae86
Triimeyazzz <trimei.astuti06@gmail.com>
2024-07-15 13:39:11 +0700

    change the tables

--- database/migrations/2024_07_09_082840_create_petugas_table.php
+++ database/migrations/2024_07_09_082840_create_petugas_table.php
@@ -1,8 +1,10 @@
+<?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Petugas extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -27,4 +29,4 @@ class Petugas extends Migration
     {
         Schema::dropIfExists('petugas');
     }
-}
+};
